3dc7d85ebd1f645b28a57042a62496a411b4757e
Syphontwo <mikey.awbrey@gmail.com>
2018-02-01 13:44:14 -0600

    this vs jQuery this
    Pretty big discovery and some fun learning with performance timers

--- Playground/indexScript.js
+++ Playground/indexScript.js
@@ -1,3 +1,28 @@
 $(function(){
-  $("h1").text("hello world");
+  var now, later;
+  var dummy;
+  now = performance.now();
+  for (var x = 0; x < 10000; x++){
+    dummy = this;
+  }
+  later = performance.now();
+  var thisTest = later - now;
+  $('body').append('<p id="ThisTest">Start of this: ' + now + '\nEnd of this: ' + later + '\nDifference: ' + thisTest + '</p>');
+
+  now = performance.now();
+  for (var x = 0; x < 10000; x++){
+    dummy = $(this);
+  }
+  later = performance.now();
+  var dollarThisTest = later - now;
+  $('body').append('<p id="DollarThisTest">Start of $(this): ' + now + '\nEnd of $(this): ' + later + '\nDifference: ' + dollarThisTest + '</p>');
+
+  now = performance.now();
+  for (var x = 0; x < 10000; x++){
+    dummy = $(this.name);
+  }
+  later = performance.now();
+  var dollarThisToStringTest = later - now;
+  $('body').append('<p id="DollarThisTest">Start of $(this.name): ' + now + '\nEnd of $(this.name): ' + later + '\nDifference: ' + dollarThisToStringTest + '</p>');
+
 });
