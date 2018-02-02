09b6596e51fc329c4d029c8bf9a926704c840464
Syphontwo <mikey.awbrey@gmail.com>
2018-02-02 15:49:00 -0600

    20-pixle art version 0.1
    Working!!!!
    
    WOO!  now to make it pretty

--- Lesson 21/From Scratch/PixleArtScript.js
+++ Lesson 21/From Scratch/PixleArtScript.js
@@ -1,27 +1,68 @@
 function updateCanvas(){
-  var rowsCount = Math.max($('.selectionForm input[name=rows]').val(), 1);
-  var columnsCount = Math.max($('.selectionForm input[name=columns]').val(), 1);
+  var CellsSetting = Math.max($('.selectionForm input[name=cells]').val(), 1);
+  var rows = setRows(CellsSetting);
+}
 
-  var cellWidth = $('.canvas').width() / rowsCount;
-  var cellHeight = $('.canvas').height() / columnsCount;
+function setRows(desiredRows){
+  if ($('.rowBlock').length == false){
+    $('.canvas').append("<div class='rowBlock'></div>");
+  }
 
-  for (var x = 1; x <= rowsCount; x++){
-    for (var y = 1; y <= columnsCount; y++){
-      $('.canvas')
+  while($('.rowBlock').length < desiredRows){
+      $('.canvas').append("<div class='rowBlock'></div>");
+    }
+
+  while($('.rowBlock').length > desiredRows){
+    $('.rowBlock').last().remove();
+  }
+
+  var setCells = function(){
+    var row = $(this);
+    var columns = row.find('.columnCell').length ? row.find('.columnCell').length : 0;
+
+    while(columns < desiredRows){
+      row.append("<span class='columnCell cellBorder'></span>");
+      row = $(this);
+      columns = $(this).find('.columnCell').length;
+    }
+
+    while(columns > desiredRows){
+      row.find('.columnCell').last().remove();
+      row = $(this);
+      columns = $(this).find('.columnCell').length;
+    }
+  };
+
+  $('.rowBlock').each(setCells);
+
+  return $('.rowBlock');
+}
+
+function changeColor(eventArgs){
+  if (mouseIsDown){
+    $(eventArgs.target).attr('style', "background-color:" + $('.selectionForm input[name=color]').val());
+    if($(eventArgs.target).hasClass("cellBorder")){
+       $(eventArgs.target).removeClass("cellBorder");
+       $(eventArgs.target).addClass("clearBorder");
     }
   }
 }
 
-
-
 function init(){
-  $('.selectionForm input[name=rows]').change(updateCanvas);
-  $('.selectionForm input[name=columns]').change(updateCanvas);
-  $(window).resize(updateCanvas);
+  $('.selectionForm input[name=cells]').change(updateCanvas);
+  $(document).mousedown(function(){mouseIsDown = true;})
+  $(document).mouseup(function(){mouseIsDown = false;})
 
-  $('.selectionForm input[name=rows]').val(20);
-  $('.selectionForm input[name=columns]').val(20);
+  $('.canvas').on('mouseenter', '*', changeColor);
+
+  $('.selectionForm input[name=cells]').val(50);
   $('.selectionForm input[name=color]').val('#6B5B95');
+
+console.log('start');
+  $('.loadingMessage').remove();
+  updateCanvas();
 }
+
+var mouseIsDown = false;
 
 $(init);
