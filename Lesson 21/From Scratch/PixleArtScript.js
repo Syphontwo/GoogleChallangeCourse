ec4b0ebb3aacbc2640b8aee3922abf3686bde9cd
Syphontwo <mikey.awbrey@gmail.com>
2018-02-05 09:53:07 -0600

    cool title
    cool title stuff added.  Gotta think of some more fun interactions, though it is drawable

--- Lesson 21/From Scratch/PixleArtScript.js
+++ Lesson 21/From Scratch/PixleArtScript.js
@@ -1,45 +1,45 @@
 function updateCanvas(){
   var CellsSetting = Math.max($('.selectionForm input[name=cells]').val(), 1);
-  var rows = setRows(CellsSetting);
+  var rows = setRows(CellsSetting, CellsSetting, $('#userArt'));
 }
 
-function setRows(desiredRows){
-  if ($('.rowBlock').length == false){
-    $('.canvas').append("<div class='rowBlock'></div>");
+function setRows(desiredRows, desiredColumns, baseElement){
+  if ($(baseElement).find('.rowBlock').length == false){
+    $(baseElement).append("<div class='rowBlock'></div>");
   }
 
-  while($('.rowBlock').length < desiredRows){
-      $('.canvas').append("<div class='rowBlock'></div>");
+  while($(baseElement).find('.rowBlock').length < desiredRows){
+      $(baseElement).append("<div class='rowBlock'></div>");
     }
 
-  while($('.rowBlock').length > desiredRows){
-    $('.rowBlock').last().remove();
+  while($(baseElement).find('.rowBlock').length > desiredRows){
+    $(baseElement).find('.rowBlock').last().remove();
   }
 
   var setCells = function(){
     var row = $(this);
     var columns = row.find('.columnCell').length ? row.find('.columnCell').length : 0;
 
-    while(columns < desiredRows){
+    while(columns < desiredColumns){
       row.append("<span class='columnCell cellBorder'></span>");
       row = $(this);
       columns = $(this).find('.columnCell').length;
     }
 
-    while(columns > desiredRows){
+    while(columns > desiredColumns){
       row.find('.columnCell').last().remove();
       row = $(this);
       columns = $(this).find('.columnCell').length;
     }
   };
 
-  $('.rowBlock').each(setCells);
+  $(baseElement).find('.rowBlock').each(setCells);
 
-  return $('.rowBlock');
+  return $(baseElement).find('.rowBlock');
 }
 
 function changeColor(eventArgs){
-  if (mouseIsDown){
+  if (mouseIsDown || (eventArgs.type === 'click' && $(eventArgs.target).hasClass('columnCell'))){
     $(eventArgs.target).attr('style', "background-color:" + $('.selectionForm input[name=color]').val());
     if($(eventArgs.target).hasClass("cellBorder")){
        $(eventArgs.target).removeClass("cellBorder");
@@ -48,19 +48,48 @@ function changeColor(eventArgs){
   }
 }
 
+function titleArt(){
+  var rows = setRows(5, 33, $('#titleArt'));
+  randomTitle();
+}
+
+function randomTitle(){
+  var colorR = Math.floor((Math.random() * 256));
+  var colorG = Math.floor((Math.random() * 256));
+  var colorB = Math.floor((Math.random() * 256));
+  var color = "background-color:" + "rgb(" + colorR + "," + colorG + "," + colorB + ")";
+  var pixelRows = $('#titleArt').find('.rowBlock');
+  var setColor = function(cells){
+    $(cells).attr('style', color);
+  }
+
+  //row by row set of title colors to say "pixel art"
+  var columnsCells = $(pixelRows.get(0)).find('.columnCell');
+  setColor([columnsCells[0],columnsCells[1],columnsCells[2],columnsCells[4], columnsCells[5], columnsCells[6], columnsCells[8], columnsCells[10], columnsCells[12], columnsCells[13],columnsCells[14],columnsCells[16], columnsCells[22],columnsCells[23],columnsCells[24],columnsCells[26],columnsCells[27],columnsCells[30],columnsCells[31],columnsCells[32]]);
+  columnsCells = $(pixelRows.get(1)).find('.columnCell');
+  setColor([columnsCells[0],columnsCells[2],columnsCells[5],columnsCells[8],columnsCells[10],columnsCells[12],columnsCells[16],columnsCells[22],columnsCells[24],columnsCells[26],columnsCells[28],columnsCells[31]]);
+  columnsCells = $(pixelRows.get(2)).find('.columnCell');
+  setColor([columnsCells[0],columnsCells[1],columnsCells[2],columnsCells[5],columnsCells[9],columnsCells[12],columnsCells[13],columnsCells[14],columnsCells[16],columnsCells[22],columnsCells[23],columnsCells[24],columnsCells[26],columnsCells[27],columnsCells[31]]);
+  columnsCells = $(pixelRows.get(3)).find('.columnCell');
+  setColor([columnsCells[0],columnsCells[5],columnsCells[8],columnsCells[10],columnsCells[12],columnsCells[16],columnsCells[22],columnsCells[24],columnsCells[26],columnsCells[28],columnsCells[31]]);
+  columnsCells = $(pixelRows.get(4)).find('.columnCell');
+  setColor([columnsCells[0],columnsCells[4],columnsCells[5],columnsCells[6],columnsCells[8],columnsCells[10],columnsCells[12],columnsCells[13],columnsCells[14],columnsCells[16],columnsCells[17],columnsCells[18],columnsCells[22],columnsCells[24],columnsCells[26],columnsCells[28],columnsCells[31]]);
+}
+
 function init(){
   $('.selectionForm input[name=cells]').change(updateCanvas);
   $(document).mousedown(function(){mouseIsDown = true;})
   $(document).mouseup(function(){mouseIsDown = false;})
 
+  $('.canvas').on('click', changeColor);
   $('.canvas').on('mouseenter', '*', changeColor);
 
   $('.selectionForm input[name=cells]').val(50);
   $('.selectionForm input[name=color]').val('#6B5B95');
 
-console.log('start');
   $('.loadingMessage').remove();
   updateCanvas();
+  titleArt();
 }
 
 var mouseIsDown = false;
